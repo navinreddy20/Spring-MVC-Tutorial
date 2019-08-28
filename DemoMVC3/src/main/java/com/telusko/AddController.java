e8faf78e091bff783c90d205daa39b411bf43265
Telusko <Telusko@SuperComputer>
2019-08-28 15:54:02 +0530

    Spring MVC Tutorial with Example part 4

---
+++ DemoMVC3/src/main/java/com/telusko/AddController.java
@@ -0,0 +1,26 @@
+package com.telusko;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController 
+{
+	@RequestMapping("/add")
+	public ModelAndView add(HttpServletRequest request,HttpServletResponse response) 
+	{
+		int i=Integer.parseInt(request.getParameter("t1"));
+		int j=Integer.parseInt(request.getParameter("t2"));
+		int k=i+j;
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result",k);
+		
+		return mv;
+	}
+}
